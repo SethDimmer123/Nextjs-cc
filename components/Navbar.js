8901aca81775d7f1d9c58e5f867382d305114ef3
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-03 13:52:24 -0400

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import styles from "../styles/Navbar.module.css"
 
 export default function Navbar() {
+    console.log(styles)
     return (
-        <nav>
-            <ul>
+        <nav className={styles.nav}>
+            <ul className={styles.ul}>
                 <li>Home</li>
                 <li>Posts</li>
                 <li>Users</li>
