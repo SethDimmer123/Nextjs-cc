7dcc8cd9302977d7f22c88d3f440bfb27f6a72b5
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-03 14:29:05 -0400

    initial commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,9 @@ export default function Navbar() {
     return (
         <nav className={styles.nav}>
             <ul className={styles.ul}>
+                <Link href="/">
                 <li>Home</li>
+                </Link>
                 <Link href="/posts">
                 <li>Posts</li>
                 </Link>
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Navbar from '@/components/Navbar'
 export default function Home() {
   return (
     <>
-    <Navbar />
     <h1>This is the home page</h1>
     </>
   )
