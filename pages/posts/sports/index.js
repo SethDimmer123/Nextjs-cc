6d6c6ade70a23fc91d4f04415bc9c72f8765c78d
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-04 11:04:29 -0400

    initial commit

---
+++ pages/posts/sports/index.js
@@ -0,0 +1,15 @@
+export default function Jack(){
+    return <>
+    <h1>this is the sports page</h1>
+    </>
+}
+
+/**
+ * 
+ * 
+ * 
+ * /posts/sports/trending/jack
+ * 
+ * 
+ * 
+ */
---
+++ pages/posts/sports/trending/index.js
@@ -0,0 +1,15 @@
+export default function Jack(){
+    return <>
+    <h1>this is the trending page</h1>
+    </>
+}
+
+/**
+ * 
+ * 
+ * 
+ * /posts/sports/trending/jack
+ * 
+ * 
+ * 
+ */
