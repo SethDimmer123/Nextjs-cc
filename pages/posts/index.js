897a6d77692c733becc93af2d440767e3654275a
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-04 14:20:42 -0400

    initial commit

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,12 +1,36 @@
-export default function Posts() {
+import { useEffect,useState } from "react"
+
+
+// all rendered on the server side
+
+// not on the client side
+
+
+
+export async function getServerSideProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await response.json()
+
+    return {
+        props: {
+            posts : data
+        }
+    }
+}
+
+
+
+
+
+
+
+export default function Posts({posts}) {
     return (
         <>
         <h1>This is the posts page</h1>
+        {posts?.map(post => {
+            return <li key={post.id}>{post.title}</li>
+        })}
         </>
     )
 }
-
-/**
- * /users
- * /posts
- */
